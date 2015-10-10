c143a29e18f7b307f84c60f6213b5f9786766114
Sankha Narayan Guria <sankha93@gmail.com>
2015-10-10 17:20:48 +0530

    some more automation

--- dashboard.sh
+++ dashboard.sh
@@ -2,7 +2,11 @@
 
 python wakatime.py
 cat base_script.js >> _script.js
+git stash
 git checkout gh-pages
 mv _script.js script.js
 git add script.js
 git commit -m "Update dashboard at `date`"
+git push -f origin gh-pages
+git checkout master
+git stash pop
