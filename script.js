b9b2c6ce696dccd60351f6dc457d0d2ff5882592
Sankha Narayan Guria <sankha93@gmail.com>
2016-02-14 17:29:42 +0530

    Update dashboard at Sun Feb 14 17:29:42 IST 2016

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var data = {"labels": ["7 February", "8 February", "9 February", "10 February", "11 February", "12 February", "13 February"], "datasets": [{"languages": [["JavaScript", "Ruby", "Python"], ["Python", "Ruby", "JavaScript"], ["JavaScript", "Python", "JSON"], ["Python", "Ruby", "JavaScript"], ["Ruby", "Text", "Other"], [], ["Python", "JavaScript"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["5 hrs 15 mins", "6 hrs 30 mins", "7 hrs 15 mins", "4 hrs 15 mins", "1 hr 12 mins", "0 secs", "17 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [5.290555555555556, 6.530277777777778, 7.283611111111111, 4.282777777777778, 1.27, 0.0, 0.30583333333333335], "label": "Dataset"}]};
+var totalTime = "24 hours 57 minutes";
+var languages = ["Python", "JavaScript", "Ruby"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
