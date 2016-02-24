750444eefc5e2f95b2cb88ce39a0f850c1bf749a
Sankha Narayan Guria <sankha93@gmail.com>
2016-02-24 13:51:16 +0530

    Update dashboard at Wed Feb 24 13:51:16 IST 2016

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var data = {"labels": ["16 February", "17 February", "18 February", "19 February", "20 February", "21 February", "22 February"], "datasets": [{"languages": [["JavaScript", "Python", "Ruby"], ["Ruby", "Python", "JavaScript"], ["Java", "JavaScript", "Python"], ["Markdown", "Java", "XML"], [], ["Ruby", "Python"], ["Ruby"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["3 hrs 9 mins", "2 hrs 14 mins", "4 hrs 23 mins", "5 hrs 5 mins", "0 secs", "22 mins", "28 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [3.2202777777777776, 2.290277777777778, 4.462777777777778, 5.12, 0.0, 0.39916666666666667, 0.485], "label": "Dataset"}]};
-var totalTime = "15 hours 58 minutes";
-var languages = ["Ruby", "Java", "JavaScript"];
+var data = {"labels": ["17 February", "18 February", "19 February", "20 February", "21 February", "22 February", "23 February"], "datasets": [{"languages": [["Ruby", "Python", "JavaScript"], ["Java", "JavaScript", "Python"], ["Markdown", "Java", "XML"], [], ["Ruby", "Python"], ["Ruby"], ["JavaScript", "Ruby", "Perl"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["2 hrs 14 mins", "4 hrs 23 mins", "5 hrs 5 mins", "0 secs", "22 mins", "42 mins", "21 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [2.290277777777778, 4.462777777777778, 5.12, 0.0, 0.39916666666666667, 0.7325, 0.3680555555555556], "label": "Dataset"}]};
+var totalTime = "13 hours 22 minutes";
+var languages = ["Ruby", "Java", "Markdown"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
