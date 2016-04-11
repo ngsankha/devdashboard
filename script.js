b9bc66af99d55c69aeec7b2ec0e847dcea49f03d
Sankha Narayan Guria <sankha93@gmail.com>
2016-04-11 15:24:27 +0530

    Update dashboard at Mon Apr 11 15:24:27 IST 2016

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var data = {"labels": ["4 April", "5 April", "6 April", "7 April", "8 April", "9 April", "10 April"], "datasets": [{"languages": [["Bash", "Java", "JavaScript"], ["JavaScript", "Ruby"], ["JavaScript", "Ruby"], ["JavaScript"], [], [], []], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["4 hrs 5 mins", "9 mins", "37 mins", "19 mins", "0 secs", "0 secs", "0 secs"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [4.115833333333334, 0.16805555555555557, 0.6372222222222222, 0.3375, 0.0, 0.0, 0.0], "label": "Dataset"}]};
+var totalTime = "5 hours 15 minutes";
+var languages = ["Bash", "JavaScript", "Java"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
