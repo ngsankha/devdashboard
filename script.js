5b677b47f83c8dc17fbce2e8d5cfbb50e14b1773
Sankha Narayan Guria <sankha93@gmail.com>
2016-04-28 12:27:44 +0530

    Update dashboard at Thu Apr 28 12:27:44 IST 2016

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var data = {"labels": ["20 April", "21 April", "22 April", "23 April", "24 April", "25 April", "26 April"], "datasets": [{"languages": [["Python", "YAML", "JavaScript"], ["Ruby", "JavaScript", "Makefile"], [], ["Other", "GAS", "C"], ["Bash", "Makefile", "Batchfile"], ["Bash", "Ruby", "JavaScript"], ["Ruby", "Bash"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["41 mins", "30 mins", "0 secs", "1 hr 28 mins", "4 hrs 51 mins", "1 hr 20 mins", "40 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.7244444444444444, 0.5263888888888889, 0.0, 1.475, 4.865277777777778, 1.3711111111111112, 0.6794444444444444], "label": "Dataset"}]};
-var totalTime = "9 hours 38 minutes";
-var languages = ["Bash", "Ruby", "Makefile"];
+var data = {"labels": ["21 April", "22 April", "23 April", "24 April", "25 April", "26 April", "27 April"], "datasets": [{"languages": [["Ruby", "JavaScript", "Makefile"], [], ["Other", "GAS", "C"], ["Bash", "Makefile", "Batchfile"], ["Bash", "Ruby", "JavaScript"], ["Ruby", "Bash", "JavaScript"], ["Ruby", "Bash"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["30 mins", "0 secs", "1 hr 28 mins", "4 hrs 51 mins", "1 hr 20 mins", "3 hrs 47 mins", "12 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.5263888888888889, 0.0, 1.475, 4.865277777777778, 1.3711111111111112, 3.8513888888888888, 0.20027777777777778], "label": "Dataset"}]};
+var totalTime = "12 hours 17 minutes";
+var languages = ["Bash", "Ruby", "JavaScript"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
