e7908d1fdc53e481c315b8ecb3c57c3a66a33c59
Sankha Narayan Guria <sankha93@gmail.com>
2016-04-30 01:25:10 +0530

    Update dashboard at Sat Apr 30 01:25:10 IST 2016

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var data = {"labels": ["22 April", "23 April", "24 April", "25 April", "26 April", "27 April", "28 April"], "datasets": [{"languages": [[], ["Other", "GAS", "C"], ["Bash", "Makefile", "Batchfile"], ["Bash", "Ruby", "JavaScript"], ["Ruby", "Bash", "JavaScript"], ["Ruby", "JavaScript", "Logos"], ["Ruby", "JavaScript", "JSON"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["0 secs", "1 hr 28 mins", "4 hrs 51 mins", "1 hr 20 mins", "3 hrs 47 mins", "2 hrs 10 mins", "38 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.0, 1.475, 4.865277777777778, 1.3711111111111112, 3.8513888888888888, 2.2191666666666667, 0.6611111111111111], "label": "Dataset"}]};
-var totalTime = "14 hours 26 minutes";
-var languages = ["Bash", "Ruby", "JavaScript"];
+var data = {"labels": ["23 April", "24 April", "25 April", "26 April", "27 April", "28 April", "29 April"], "datasets": [{"languages": [["Other", "GAS", "C"], ["Bash", "Makefile", "Batchfile"], ["Bash", "Ruby", "JavaScript"], ["Ruby", "Bash", "JavaScript"], ["Ruby", "JavaScript", "Logos"], ["Ruby", "JavaScript", "Bash"], []], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["1 hr 28 mins", "4 hrs 51 mins", "1 hr 20 mins", "3 hrs 47 mins", "2 hrs 10 mins", "2 hrs 38 mins", "0 secs"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [1.475, 4.865277777777778, 1.3711111111111112, 3.8513888888888888, 2.2191666666666667, 2.667222222222222, 0.0], "label": "Dataset"}]};
+var totalTime = "16 hours 26 minutes";
+var languages = ["Ruby", "Bash", "JavaScript"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
