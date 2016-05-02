8dcf3fef94f4ae58726bdb5380108aaa21d06236
Sankha Narayan Guria <sankha93@gmail.com>
2016-05-03 01:34:08 +0530

    Update dashboard at Tue May  3 01:34:08 IST 2016

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var data = {"labels": ["25 April", "26 April", "27 April", "28 April", "29 April", "30 April", "1 May"], "datasets": [{"languages": [["Bash", "Ruby", "JavaScript"], ["Ruby", "Bash", "JavaScript"], ["Ruby", "JavaScript", "Logos"], ["Ruby", "JavaScript", "Bash"], ["Rust"], [], ["Ruby", "JavaScript", "Other"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["1 hr 20 mins", "3 hrs 47 mins", "2 hrs 10 mins", "2 hrs 38 mins", "2 mins", "0 secs", "1 hr 51 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [1.3711111111111112, 3.8513888888888888, 2.2191666666666667, 2.667222222222222, 0.036944444444444446, 0.0, 1.8833333333333333], "label": "Dataset"}]};
-var totalTime = "12 hours 01 minutes";
-var languages = ["Ruby", "Bash", "JavaScript"];
+var data = {"labels": ["26 April", "27 April", "28 April", "29 April", "30 April", "1 May", "2 May"], "datasets": [{"languages": [["Ruby", "Bash", "JavaScript"], ["Ruby", "JavaScript", "Logos"], ["Ruby", "JavaScript", "Bash"], ["Rust"], [], ["Ruby", "JavaScript", "Other"], ["PHP"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["3 hrs 47 mins", "2 hrs 10 mins", "2 hrs 38 mins", "2 mins", "0 secs", "2 hrs 57 mins", "18 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [3.8513888888888888, 2.2191666666666667, 2.667222222222222, 0.036944444444444446, 0.0, 2.985, 0.3086111111111111], "label": "Dataset"}]};
+var totalTime = "12 hours 04 minutes";
+var languages = ["Ruby", "JavaScript", "Bash"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
