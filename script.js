ce1ebb57b254f75036546dccdb5ede9a952c5f52
Sankha Narayan Guria <sankha93@gmail.com>
2016-05-08 15:59:25 +0530

    Update dashboard at Sun May  8 15:59:25 IST 2016

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var data = {"labels": ["1 May", "2 May", "3 May", "4 May", "5 May", "6 May", "7 May"], "datasets": [{"languages": [["Ruby", "JavaScript", "Other"], ["Ruby", "JavaScript", "PHP"], ["Ruby", "JavaScript"], ["Ruby", "JavaScript", "Bash"], ["Ruby", "Bash", "Other"], [], ["Rust", "Other"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["2 hrs 57 mins", "1 hr 42 mins", "0 secs", "35 mins", "34 mins", "0 secs", "2 hrs 3 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [2.985, 1.7358333333333333, 0.0044444444444444444, 0.6066666666666667, 0.5847222222222223, 0.0, 2.0597222222222222], "label": "Dataset"}]};
-var totalTime = "7 hours 58 minutes";
-var languages = ["Ruby", "Rust", "JavaScript"];
+var data = {"labels": ["1 May", "2 May", "3 May", "4 May", "5 May", "6 May", "7 May"], "datasets": [{"languages": [["Ruby", "JavaScript", "Other"], ["Ruby", "JavaScript", "PHP"], ["Ruby", "JavaScript"], ["Ruby", "JavaScript", "Bash"], ["Ruby", "Bash", "Other"], [], ["Rust", "Markdown", "Other"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["2 hrs 57 mins", "1 hr 42 mins", "0 secs", "35 mins", "34 mins", "0 secs", "5 hrs 8 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [2.985, 1.7358333333333333, 0.0044444444444444444, 0.6066666666666667, 0.5847222222222223, 0.0, 5.137222222222222], "label": "Dataset"}]};
+var totalTime = "11 hours 03 minutes";
+var languages = ["Rust", "Ruby", "JavaScript"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
