fc669af60493bd45da93a3a171ec74a7bc71f6d7
Sankha Narayan Guria <sankha93@gmail.com>
2016-05-11 12:17:08 +0530

    Update dashboard at Wed May 11 12:17:08 IST 2016

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var data = {"labels": ["3 May", "4 May", "5 May", "6 May", "7 May", "8 May", "9 May"], "datasets": [{"languages": [["Ruby", "JavaScript"], ["Ruby", "JavaScript", "Bash"], ["Ruby", "Bash", "Other"], [], ["Rust", "Markdown", "Other"], ["Ruby", "Python", "Java"], ["Ruby", "Bash", "JavaScript"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["0 secs", "35 mins", "34 mins", "0 secs", "5 hrs 8 mins", "1 hr 30 mins", "2 hrs 11 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.0044444444444444444, 0.6066666666666667, 0.5847222222222223, 0.0, 5.137222222222222, 1.5266666666666666, 2.2044444444444444], "label": "Dataset"}]};
-var totalTime = "10 hours 03 minutes";
-var languages = ["Rust", "Ruby", "Bash"];
+var data = {"labels": ["4 May", "5 May", "6 May", "7 May", "8 May", "9 May", "10 May"], "datasets": [{"languages": [["Ruby", "JavaScript", "Bash"], ["Ruby", "Bash", "Other"], [], ["Rust", "Markdown", "Other"], ["Ruby", "Python", "Java"], ["Ruby", "JavaScript", "Python"], ["Python"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["35 mins", "34 mins", "0 secs", "5 hrs 8 mins", "1 hr 30 mins", "3 hrs 12 mins", "13 mins"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.6066666666666667, 0.5847222222222223, 0.0, 5.137222222222222, 1.5266666666666666, 3.2222222222222223, 0.2175], "label": "Dataset"}]};
+var totalTime = "11 hours 17 minutes";
+var languages = ["Rust", "Ruby", "Python"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
