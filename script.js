285d7a1e125278dd02660e9512cda4662ab57692
Sankha Narayan Guria <sankha93@gmail.com>
2016-06-13 00:38:21 +0530

    Update dashboard at Mon Jun 13 00:38:21 IST 2016

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var data = {"labels": ["5 June", "6 June", "7 June", "8 June", "9 June", "10 June", "11 June"], "datasets": [{"languages": [[], [], [], [], [], ["Other"], []], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["0 secs", "0 secs", "0 secs", "0 secs", "0 secs", "0 secs", "0 secs"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "label": "Dataset"}]};
+var data = {"labels": ["6 June", "7 June", "8 June", "9 June", "10 June", "11 June", "12 June"], "datasets": [{"languages": [[], [], [], [], ["Other"], [], []], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["0 secs", "0 secs", "0 secs", "0 secs", "0 secs", "0 secs", "0 secs"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "label": "Dataset"}]};
 var totalTime = "0 hours 00 minutes";
 var languages = ["Other"];
 var ctx = document.getElementById("chart").getContext("2d");
