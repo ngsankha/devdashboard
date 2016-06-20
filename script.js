a3cdc6c43bd05aaa06ffa7c16b953092fe4cad64
Sankha Narayan Guria <sankha93@gmail.com>
2016-06-20 09:01:26 +0530

    Update dashboard at Mon Jun 20 09:01:26 IST 2016

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var data = {"labels": ["12 June", "13 June", "14 June", "15 June", "16 June", "17 June", "18 June"], "datasets": [{"languages": [["Ruby", "JavaScript", "Java"], ["Ruby", "JavaScript", "Markdown"], ["Bash", "Markdown", "Ruby"], ["Bash", "JavaScript", "Ruby"], ["Ruby", "Bash", "JavaScript"], [], []], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["2 hrs 33 mins", "1 hr 43 mins", "38 mins", "1 hr 36 mins", "38 mins", "0 secs", "0 secs"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [2.5858333333333334, 1.7561111111111112, 0.6544444444444445, 1.6316666666666666, 0.6558333333333334, 0.0, 0.0], "label": "Dataset"}]};
-var totalTime = "7 hours 17 minutes";
-var languages = ["Ruby", "JavaScript", "Bash"];
+var data = {"labels": ["13 June", "14 June", "15 June", "16 June", "17 June", "18 June", "19 June"], "datasets": [{"languages": [["Ruby", "JavaScript", "Markdown"], ["Bash", "Markdown", "Ruby"], ["Bash", "JavaScript", "Ruby"], ["Ruby", "Bash", "JavaScript"], [], ["Rust"], ["Markdown"]], "pointHighlightFill": "#fff", "fillColor": "rgba(151,187,205,0.2)", "pointHighlightStroke": "rgba(151,187,205,1)", "time": ["1 hr 43 mins", "38 mins", "1 hr 36 mins", "38 mins", "0 secs", "54 mins", "0 secs"], "pointColor": "rgba(151,187,205,1)", "strokeColor": "rgba(151,187,205,1)", "pointStrokeColor": "#fff", "data": [1.7561111111111112, 0.6544444444444445, 1.6316666666666666, 0.6558333333333334, 0.0, 0.9047222222222222, 0.005555555555555556], "label": "Dataset"}]};
+var totalTime = "5 hours 36 minutes";
+var languages = ["Ruby", "Bash", "JavaScript"];
 var ctx = document.getElementById("chart").getContext("2d");
 var myLineChart = new Chart(ctx).Line(data, {
   tooltipTemplate: function(valuesObject){
